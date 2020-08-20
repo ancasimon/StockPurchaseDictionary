18913e82275324bf6db8edfb6acd278583d01f69
Anca Simon <ancarsimon@gmail.com>
2020-08-20 15:45:28 -0500

    Add project files.

---
+++ StockPurchaseDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockPurchaseDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stocks = new Dictionary<string, string>();
+            stocks.Add("GM", "General Motors");
+            stocks.Add("CAT", "Caterpillar");
+            stocks.Add("AA", "Alcoa");
+            stocks.Add("F", "Ford");
+            stocks.Add("AAPL", "Apple Computer");
+            stocks.Add("AET", "Aetna");
+
+            string GM = stocks["GM"];
+        }
+    }
+}
